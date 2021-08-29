709e2fe56c934e19bf3c9a1651ed8ac2db985c5a
Angelica Sargent <agraysargent@gmail.com>
2021-08-29 13:37:18 -0400

    fixed store typos and curly braces

---
+++ client/build/precache-manifest.48c7180ff579dda04e04f2d4c534bce2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "166802c37829d570536b192c944ef225",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cfa7c8b21c0537dcc5b7",
+    "url": "/static/css/main.a64bd9e7.chunk.css"
+  },
+  {
+    "revision": "00cf4b152aa6a72508e4",
+    "url": "/static/js/2.751d4510.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/static/js/2.751d4510.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfa7c8b21c0537dcc5b7",
+    "url": "/static/js/main.78336bc5.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
